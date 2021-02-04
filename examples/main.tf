e4accd17679e28b4e53e0907a81c9d1f394782cd
Oscar Patricio <ext.oscar.patricio@farfetch.com>
2021-02-04 21:43:54 +0000

    fix: fix example

--- examples/main.tf
+++ examples/main.tf
@@ -2,23 +2,19 @@ provider "azurerm" {
   features {}
 }
 
-resource "random_id" "rg_name" {
-  byte_length = 8
-}
-
-resource "random_id" "pip_name" {
+resource "random_id" "example_id" {
   byte_length = 8
 }
 
 resource "azurerm_resource_group" "example_rg" {
-  name     = "example-${random_id.rg_name.hex}-rg"
+  name     = "example-${random_id.example_id.hex}-rg"
   location = "northeurope"
 }
 
 module "pip" {
   source = "../"
 
-  name                = "example-${random_id.pip_name.hex}-pip"
+  name                = "example-${random_id.example_id.hex}-pip"
   resource_group_name = azurerm_resource_group.example_rg.name
   location            = "northeurope"
   allocation_method   = "static"
